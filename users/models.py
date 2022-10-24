e38e1b7972c2bfc27ea1a1dfa92129516b41547a
nealwaga <waganeal@gmail.com>
2022-10-24 20:44:48 +0300

    created Profile table

--- users/models.py
+++ users/models.py
@@ -5,3 +5,10 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField (User, on_delete=models.CASCADE)
+    image = CloudinaryField ('image')
+    name = models.CharField (blank=False, max_length=100)
+    bio = models.CharField (blank=True, max_length=65)
+    email = models.EmailField (blank=False, max_length=100)
+    income = models.IntegerField (default=0)
