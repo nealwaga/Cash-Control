40974732a1385bb260b0ed306f7ba9da07c3108b
nealwaga <waganeal@gmail.com>
2022-10-24 20:55:04 +0300

    created Profile table

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from cloudinary.models import CloudinaryField
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField (User, on_delete=models.CASCADE)
@@ -12,3 +13,21 @@ class Profile(models.Model):
     bio = models.CharField (blank=True, max_length=65)
     email = models.EmailField (blank=False, max_length=100)
     income = models.IntegerField (default=0)
+
+
+    @property
+    def username(self):
+        return self.user.username
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
+    @receiver (post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver (post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
