30c0f81085bdf78cb525c774813157723efae61d
nealwaga <waganeal@gmail.com>
2022-10-26 13:33:40 +0300

    added models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,27 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User 
+from unicodedata import category
 
 # Create your models here.
+class Categories (models.Model):
+    catrgory = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.category 
+
+
+class EXpenses(models.Model):
+    user = models.ForeignKey (User, on_delete=models.CASCADE)
+    category = models.ForeignKey (Categories, on_delete=models.CASCADE)
+    description = models.CharField (max_length=50)
+    amount =  models.IntegerField (default=0)
+    date = models.DateTimeField (auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.description
+
+    @classmethod
+    def search_by_description(cls, search_term):
+        captions = cls.objects.filter(description_icontains=search_term)
+        return captions
