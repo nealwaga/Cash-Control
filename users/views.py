3e88983fddf6d161ac19caf6c5cd5eb8e3e969b1
nealwaga <waganeal@gmail.com>
2023-01-12 21:30:31 +0300

    edited sign up form

--- users/views.py
+++ users/views.py
@@ -26,47 +26,47 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 
-# def register(request):
-#       #put a check in place
-#     if request.method =='POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-            
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data['password1']
-
-#             user = authenticate(username=username, password=password)
-#             login(request, user)
-            
-#             return redirect('login')
-#     else:
-#         form = UserCreationForm(request.POST)
-#     return render(request,'users/register.html',{'form':form})  
-
 def register(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
+      #put a check in place
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            new_user = form.save()
-            # Profile.get_or_create(user=request.user)
+            form.save()
+            
             username = form.cleaned_data.get('username')
-            print(username)
+            password = form.cleaned_data['password1']
 
-            # Automatically Log In The User
-            new_user = authenticate(username=form.cleaned_data['username'],
-                                    password=form.cleaned_data['password1'],)
-            print(new_user)
-            login(request, new_user)
-            # return redirect('editprofile')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            
             return redirect('login')
+    else:
+        form = UserCreationForm(request.POST)
+    return render(request,'users/register.html',{'form':form})  
+
+# def register(request):
+#     if request.method == "POST":
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             # Profile.get_or_create(user=request.user)
+#             username = form.cleaned_data.get('username')
+#             print(username)
+
+#             # Automatically Log In The User
+#             user = authenticate(username=form.cleaned_data['username'],
+#                                     password=form.cleaned_data['password1'],)
+#             print(user)
+#             login(request, user)
+#             # return redirect('editprofile')
+#             return redirect('login')
             
 
-    elif request.user.is_authenticated:
-        return redirect('login')
-    else:
-        form = UserRegisterForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'users/register.html', context)
+#     elif request.user.is_authenticated:
+#         return redirect('login')
+#     else:
+#         form = UserRegisterForm()
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'users/register.html', context)
